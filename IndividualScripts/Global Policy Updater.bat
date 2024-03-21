f4edf66151f5511ec73ddf89cfc39ee04237596f
Jared Mingle <102331218+Retrodox@users.noreply.github.com>
2024-03-21 10:33:38 -0500

    Delete IndividualScripts directory

--- IndividualScripts/Global Policy Updater.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd C:\
-echo Running gpupdate /force in the C:\
-gpupdate /force
-echo.
-echo Command executed. Press any key to exit.
-pause >nul
--- IndividualScripts/bitdefend.ps1
+++
@@ -1,4 +0,0 @@
-Start-Process "C:\Program Files\Bitdefender\Endpoint Security\product.console"
-Start-Sleep -Seconds 2  # Wait for the console to open
-Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.SendKeys]::SendWait("StartUpdate -force{ENTER}")
